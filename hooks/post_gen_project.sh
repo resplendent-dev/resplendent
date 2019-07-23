b377be4b3c50bd5262de8450bd14e131cdf95512
Tim Gates <tim.gates@iress.com>
2019-07-23 16:50:24 +1000

    More post gen cleanup

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,4 +6,10 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP="$(dirname "${BASEDIR}")"
 
 cd "${TOP}"
-git checkout -- NEWS.rst
+git checkout -- \
+ NEWS.rst \
+ app/resplendent/__init__.py \
+ app/resplendent/filters/__init__.py \
+ app/resplendent/filters/filterdump.py \
+ app/resplendent/filters/restructuredtext.py
+ app/resplendent/version.py
