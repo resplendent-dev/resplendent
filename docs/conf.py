50e5d5f870ec36a150ea65665812b617dc3305ba
Tim Gates <tim.gates@iress.com>
2019-07-24 16:29:55 +1000

    Work out python module relative to current file.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ Configuration file for the Sphinx documentation builder.
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(
-    '..',
+    os.path.dirname(os.path.dirname(
+        os.path.abspath(sys.modules[__name__].__file__)
+    )),
     'app',
 )))
 
