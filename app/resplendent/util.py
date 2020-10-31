153ca871de2766dc1870ff52b4c813cc4ac5d258
Gary Donovan <garyd203@users.noreply.github.com>
2020-10-31 23:20:10 +1100

    Fix bug using log_exception()

--- app/resplendent/util.py
+++ app/resplendent/util.py
@@ -7,5 +7,5 @@ import logging
 from logdecorator import log_exception
 
 exclog = log_exception(  # pylint: disable=invalid-name
-    logging.ERROR, "Error handling function.", on_exceptions=(Exception,), reraise=True
+    "Error handling function.", on_exceptions=(Exception,), reraise=True
 )
