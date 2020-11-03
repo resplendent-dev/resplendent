fbdf5d3d2bdf0294941075910b028cee6c542847
Tim Gates <tim.gates@iress.com>
2020-11-04 05:47:08 +1100

    Latest updates to pipenv all

--- ci/in_docker/test.sh
+++ ci/in_docker/test.sh
@@ -11,20 +11,20 @@ source "${BASEDIR}/ci/in_docker/prepare.sh"
 cd "${BASEDIR}"
 find . -iname \*.sh -print0 | xargs -0 shellcheck
 # Version independant checks
-PYVER=3.7
-# Run pyspelling in root to check docs
-"python${PYVER}" -m pyspelling
-# Run black to check all python on 3.7 only
+PYVER=3.8
+# Run spelling in root to check docs
+"python${PYVER}" -m spelling
+# Run black to check all python on 3.8 only
 "python${PYVER}" -m black --check --diff "${BASEDIR}"
 cd "${BASEDIR}/app"
 # Version dependant checks
 for PYVER in ${PYTHONVERS} ; do
   "python${PYVER}" -m flake8 "${MODULES[@]}"
-  "python${PYVER}" -m isort -rc -c -m 3 --diff "${MODULES[@]}"
+  "python${PYVER}" -m isort -rc -c -m 3 -tc -w 88 --diff "${MODULES[@]}"
   "python${PYVER}" -m bandit -r "${MODULES[@]}"
   find "${MODULES[@]}" -iname \*.py -print0 | xargs -0 -n 1 "${BASEDIR}/ci/in_docker/pylint.sh" "python${PYVER}"
   PYTEST_FAIL="NO"
-  if ! "python${PYVER}" -m pytest -n auto --cov-config=.coveragerc --cov-fail-under=40 "--cov=${MAIN_MODULE}" --cov-report=xml:test-cov.xml --cov-report=html ; then
+  if ! "python${PYVER}" -m pytest --cov-config=.coveragerc --cov-fail-under=40 "--cov=${MAIN_MODULE}" --cov-report=xml:test-cov.xml --cov-report=html --cov-report=term-missing ; then
     PYTEST_FAIL="YES"
   fi
   if [ ! -z "${TRAVIS_JOB_ID:-}" ] ; then
